425aa6bc97738e706df500df381e68e4472e55d1
Fernando Costa Migliorini <fercosmig@gmail.com>
2024-04-27 02:27:03 -0300

    traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+struct Pessoa
+{
+    nome: String,
+    idade: i32
+}
+
+trait Voz
+{
+    fn falar(&self);
+    fn tem_voz(&self) -> bool;
+}
+
+impl Voz for Pessoa
+{
+    fn falar(&self)
+    {
+        println!("Olá, meu nome é {}", self.nome);
+    }
+    fn tem_voz(&self) -> bool
+    {
+        if self.idade > 1
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+fn main()
+{
+    let pessoa = Pessoa
+    {
+        nome: String::from("Fernando"),
+        idade: 46
+    };
+    println!("Pessoa {} pode falar? {}", pessoa.nome, pessoa.tem_voz());
 }
